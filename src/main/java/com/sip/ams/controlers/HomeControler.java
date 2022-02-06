f310e431208f8bda646db097257ff11148a268fa
marwabel <marwabelhadj22@gmail.com>
2022-02-06 16:22:27 +0100

    3ème modif

--- src/main/java/com/sip/ams/controlers/HomeControler.java
+++ src/main/java/com/sip/ams/controlers/HomeControler.java
@@ -1,12 +1,25 @@
 package com.sip.ams.controlers;
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeControler {
  
 	@RequestMapping("/info")
-	public String info() {
+	public String info(Model model) {
+		String formation = "Fullstuck";
+		
+		ArrayList certifs = new ArrayList<>();
+		certifs.add("OCA");
+		certifs.add("OCP");
+		certifs.add("Spring");
+		certifs.add("Angular");
+		model.addAttribute("certifs", certifs);
+		
+		model.addAttribute("workshop",formation);
 		System.out.println("C'est une méthode  d'information");
 	 return "Home/info";
  }
