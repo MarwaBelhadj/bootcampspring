a3e2ca9f868afccded87ebe1f082e1d79ef9117f
marwabel <marwabelhadj22@gmail.com>
2022-02-10 12:59:56 +0100

    recupération des données de la vue vers ResponseBody

--- src/main/java/com/sip/ams/controlers/HomeControler.java
+++ src/main/java/com/sip/ams/controlers/HomeControler.java
@@ -3,11 +3,20 @@ import java.util.ArrayList;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeControler {
  
+	@PostMapping("/result")
+	@ResponseBody // cette annotation te crée une fenêtre pour afficher le resulat (c'est pour tester)
+	public String result(@RequestParam("nom") String n, @RequestParam("email") String e) {
+		// return "<h1> ok </h1>"; //On peut écrire du code html  ou ajouter un style
+		return("Votre nom saisie est : "+n + "\n votre email est le suivant :  "+e) ;
+	}
 	@RequestMapping("/info")
 	public String info(Model model) {
 		String formation = "Fullstuck";
